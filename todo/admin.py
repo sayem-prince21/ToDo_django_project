c25efa7d549bfa64e7b88b57fb6e3a9daa78f154
Prince <sayemmohammadp@gmail.com>
2026-04-17 01:48:58 +0600

    made models

---
+++ todo/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Task
+# Registed my models from models.py here.
+
+admin.site.register(Task)
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todo.models import Task
 
 def home(request):
+    tasks = Task.objects.filter(is_completed=False)
+    print(tasks)
     return render(request, 'home.html')
