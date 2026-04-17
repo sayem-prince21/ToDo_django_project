dc72e6a1783b049ad2cf3125f70c0aeaddc4ce1d
Prince <sayemmohammadp@gmail.com>
2026-04-18 01:56:36 +0600

    add task functionality

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Task
-# Registed my models from models.py here.
 
-admin.site.register(Task)
+
+# cereating task list
+class TaskAdmin (admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task',)   #creating search field
+    
+# Registed my models from models.py here.
+admin.site.register(Task, TaskAdmin)
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
-    print(tasks)
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_task = Task.objects.filter(is_completed=True)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'home.html', context)
