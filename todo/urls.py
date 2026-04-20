421053a9e04647af912d71bfaad1b912265b04d4
Prince <sayemmohammadp@gmail.com>
2026-04-20 11:59:24 +0600

    added updaate task functionality

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name=('addTask')),
     path('mark_as_done/<int:pk>/', views.mark_as_done, name=('mark_as_done')),
-    path('mark_as_undone,<int:pk>/', views.mark_as_undone, name=('mark_as_undone')),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name=('mark_as_undone')),
+    path('update_task/<int:pk>/', views.update_task, name=('update_task')),
     path('delete/<int:pk>/', views.delete, name=('delete')),
 ]
--- todo/views.py
+++ todo/views.py
@@ -19,7 +19,22 @@ def mark_as_undone(request, pk):
     task.save()
     return redirect('home')
 
+def update_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context ={
+        'get_task' : get_task,
+    }
+    return render (request, 'update_task.html', context)
+
+
 def delete (request, pk):
     task = get_object_or_404 (Task, pk=pk)
     task.delete()
     return redirect('home')
+
